f30325413520bd8804c6ccb3f65c2ed8cdb98add
bkalaiselvi-02 <bkalaiselvi0211@gmail.com>
2025-06-22 22:39:40 +0530

    Create LeetCode-Day8-941-valid_mountain_array.js

---
+++ LeetCode-Day8-941-valid_mountain_array.js
@@ -0,0 +1,14 @@
+var validMountainArray = function(arr) {
+    const n = arr.length;
+    if (n < 3) return false;
+    let i = 0;
+    while (i + 1 < n && arr[i] < arr[i + 1]) {
+        i++;
+    }
+    if (i === 0 || i === n - 1) return false;
+    while (i + 1 < n && arr[i] > arr[i + 1]) {
+        i++;
+    }
+
+    return i === n - 1;
+};
